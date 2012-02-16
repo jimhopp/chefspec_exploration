241daf0865309d1c4cbb87229089f6ba9b0cae6c
Jim Hopp <jimhopp@gmail.com>
2012-02-15 23:28:13 -0800

    additional attr test

--- cookbooks/mytestcookbook/spec/attr_tests_spec.rb
+++ cookbooks/mytestcookbook/spec/attr_tests_spec.rb
@@ -1,6 +1,11 @@
 require 'chefspec'
 
 describe 'attributes tests' do
+  it 'chefspec converge picks up default attr values' do
+    runner = ChefSpec::ChefRunner.new
+    runner.converge 'mytestcookbook::default'
+    runner.node.mytestcookbook.attr_a.should eq 'default value'
+  end
   it 'can set values for default attributes' do
     runner = ChefSpec::ChefRunner.new do |node|
       node["mytestcookbook"] = { :attr_a => 'bar' }
