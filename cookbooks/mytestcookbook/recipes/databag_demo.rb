e377bf2197c3fd07afd9642b504638e72bfd5dee
Jim Hopp <jimhopp@gmail.com>
2012-02-18 07:53:42 -0800

    improve databag example

--- cookbooks/mytestcookbook/recipes/databag_demo.rb
+++ cookbooks/mytestcookbook/recipes/databag_demo.rb
@@ -3,7 +3,8 @@
 data_bag_item = data_bag_item("my_data_bag", "my_item")
 
 file "/etc/my_file" do
-  content data_bag_item["my_item"]
+  content data_bag_item["fun data"]
   mode 00644
   not_if {data_bag_item.empty?}
+  action :create # must have action, chefspec will not default action :create
 end
--- cookbooks/mytestcookbook/spec/stub_out_databag_spec.rb
+++ cookbooks/mytestcookbook/spec/stub_out_databag_spec.rb
@@ -5,6 +5,6 @@ describe 'stub out databags' do
   it 'stub out data_bag_item' do
     Chef::Recipe.any_instance.stub(:data_bag_item).and_return(Hash.new)
     Chef::Recipe.any_instance.stub(:data_bag_item).with("my_data_bag", "my_item").and_return({"id" => "my_item", "fun data" => "whatever"})
-    chef_run.should create_file('/etc/my_file')
+    chef_run.should create_file_with_content('/etc/my_file', 'whatever')
   end
 end
